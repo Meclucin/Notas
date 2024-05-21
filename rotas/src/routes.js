faf44c6b7d2daee0933bc2478930d0129da38f8f
Meclucin <lmartinskaminski@gmail.com>
2024-05-20 22:11:17 -0300

    first commit

---
+++ rotas/src/routes.js
@@ -0,0 +1,27 @@
+import { BrowserRouter, Router, Route } from 'react-router-dom' ;
+
+import Home from './pages/Home';
+import Sobre from './pages/Sobre';
+import Contato from './pages/Contato';
+import Erro from './pages/Erro';
+import Produto from './pages/Produto';
+
+import Header from './components/Header';
+
+function RoutesApp() {
+    return(
+        <BrowserRouter>
+            <header/>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/sobre" element={<Sobre/>}/>
+                    <Route path="/contato" element={<Contato/>}/>
+                    <Route path="/Produto" element={<Produto/>}/>
+                    <Route path="" element={<Erro/>}/>
+                    
+                </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default RoutesApp;
